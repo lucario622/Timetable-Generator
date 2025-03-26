48ea90679172975015a8a01183977ff986681a78
lucario622 <58982101+lucario622@users.noreply.github.com>
2025-03-25 22:49:59 -0400

    starting to understand PyQt6

--- scheduleGUI.py
+++ scheduleGUI.py
@@ -1,9 +1,9 @@
 import sys
 import ctypes
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget
 from PyQt6.QtGui import QIcon
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,18 +11,27 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
         self.setWindowIcon(QIcon("Buildings/UA/0.png"))
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('myapp.app.1')
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myapp.app.1")
 
-        button = QPushButton("My Button")
-        button.setCheckable(True)
-        button.clicked.connect(outerFunction)
+        layout = QGridLayout()
 
-        self.setFixedSize(QSize(400,800))
-
-        self.setCentralWidget(button)
+        for i in range(5):
+            for j in range(5):
+                if (i+j)%2==0:
+                    self.placeButton(layout,i,j,"Button number "+str(5*i+j+1))
+        
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
     def buttonAction(self):
         print("button be got pushed")
+        
+    def placeButton(self,layout:QGridLayout,x,y,text):
+        newbutton = QPushButton(text)
+        newbutton.clicked.connect(lambda :print(text))
+        layout.addWidget(newbutton,x,y)
+
 
 def outerFunction():
     print("outer print")
